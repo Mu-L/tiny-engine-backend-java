50f5e0b2df2bf13ec160442e5c3b7e87bb2ee49b
lu-yg <128358973+lu-yg@users.noreply.github.com>
2025-10-28 23:32:13 -0700

    fix: Modify code format (#271)

--- base/src/main/java/com/tinyengine/it/controller/AiChatController.java
+++ base/src/main/java/com/tinyengine/it/controller/AiChatController.java
@@ -35,8 +35,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import java.util.List;
-
 /**
  * The type Ai chat controller.
  *
